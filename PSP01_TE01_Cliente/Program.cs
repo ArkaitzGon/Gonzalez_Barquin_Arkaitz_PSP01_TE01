29574e8325ac477c85f6c324bba44d0f9292bbdd
Arkaitz Gonzalez <argonzalez@birt.eus>
2024-02-16 01:34:48 +0100

    Agregar archivos de proyecto.

---
+++ PSP01_TE01_Cliente/Program.cs
@@ -0,0 +1,109 @@
+﻿/**
+ * Programa cliente
+ * Recibe un enunciado
+ * Mandamos una respuesta
+ * Recibimos si hemos acertado, si hemos fallado volvemos a mandar una respuesta
+ */
+
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace pipeCliente
+{
+    class PipeCliente
+    {
+        static void Main(string[] args)
+        {
+            Process p;
+
+            // Lanzamos el proceso del servidor con el metodo que hemos creado
+            LanzaServidor(out p);
+
+            // Dejamos un tiempo prudencial de espera antes de hacer cualquier calculo
+            Task.Delay(1000).Wait();
+
+            // Preparamos el pipe del cliente para la conexion al pipe del servidor
+            var cliente = new NamedPipeClientStream("PSP01_TE01");
+
+            try
+            {
+                // Conectamos con el servidor
+                cliente.Connect(1000);
+                Console.WriteLine("Estableciendo conexion con el servidor");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Se produjo un error al intentar conectar con el servidor: {ex.Message}");
+            }
+            
+            Console.WriteLine("********************");
+            Console.WriteLine("*Comienza el juego*");
+            Console.WriteLine("********************");
+
+            // Creamos los buffers
+            StreamReader reader = new StreamReader(cliente);
+            StreamWriter writer = new StreamWriter(cliente);
+
+            
+            // Para que el programa este continuamente escuchando y leyendo datos, creamos un bucle infinito
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Resuelve el siguiente acertijo:\n");
+                Console.WriteLine("*****************Acertijo********************");
+
+                // Recibimos el enunciado
+                var enunciado = reader.ReadLine();
+                Console.WriteLine(enunciado);
+                Console.WriteLine("*********************************************");
+                Console.WriteLine("Indica una respuesta:");
+
+                // Comprobamos si ha acertado, si no se ha acertado repetimos
+                bool acierto = false;
+                while (!acierto)
+                {
+                    // Mandamos la respuesta
+                    writer.WriteLine(Console.ReadLine());
+                    writer.Flush();
+
+                    // Recibimos si hemos acertado o no
+                    var respuesta = reader.ReadLine();
+                    if (respuesta.Equals("OK"))
+                    {
+                        Console.WriteLine("¡Enhorabuena! Has acertado.");
+                        Console.WriteLine("---------***-----------");
+                        Console.WriteLine("************************");
+                        Console.WriteLine("Jugar otra vez:");
+                        Console.WriteLine("************************");
+                        acierto = true;
+                    }
+                    else if (respuesta.Equals("KO"))
+                    {
+                        Console.WriteLine("No has acertado. Introduce otra respuesta:");
+                    }
+                }
+                
+            }
+        }
+
+        /**
+         * Metodo que lanza el proceso del servidor
+         * @return Process con el proceso del servidor
+         */
+        static Process LanzaServidor(out Process p1)
+        {
+            //Iniciamos un proceso con el servidor
+            ProcessStartInfo info = new ProcessStartInfo(@"..\..\..\..\PSP01_TE01_Servidor\bin\Release\net8.0\win-x64\PSP01_TE01_Servidor.exe");
+            
+            info.CreateNoWindow = false;
+            info.WindowStyle = ProcessWindowStyle.Normal;
+
+            info.UseShellExecute = true;
+
+            // Inicia el proceso del servidor 
+            p1 = Process.Start(info);
+            return p1;
+        }
+    }
+}
+
---
+++ PSP01_TE01_Servidor/Program.cs
@@ -0,0 +1,149 @@
+﻿/**
+ * Programa Servidor
+ * Carga un fichero xml de adivinanzas (el fichero debe estar en la carpeta del primer proyecto creado en este caso cliente. Tanto en release como en debug(net8.0))
+ * Elige una al azar y envia el enunciado
+ * Recibe la respuesta y comprueba si es correcta
+ * Envia un ok si es correcto un KO si es incorrecto
+ * si es incorrecta recibe otra respusta hasta que acierte
+ */
+
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Xml;
+
+
+namespace pipeServidor
+{
+    class PipeServidor
+    {
+        static void Main(string[] args)
+        { 
+            try
+            {
+                // Cargamos el fichero con el metodo que hemos creado
+                List<Adivinanza> lista = CargaFichero();
+    
+                // Creamos el pipe del servidor
+                var server = new NamedPipeServerStream("PSP01_TE01");
+                Console.WriteLine("Conexion a servidor establecida");
+
+                // Esperamos a que el cliente contacte con el servidor
+                server.WaitForConnection();
+                
+
+                // Creamos lo buffer
+                StreamReader reader = new StreamReader(server);
+                StreamWriter writer = new StreamWriter(server);
+                Console.WriteLine("Pipe Servidor esperando datos.");
+
+                
+                while (true)
+                {
+                    Console.WriteLine("");
+                    // Creamos el numero aleatorio y lo imprimimos
+                    Random random = new Random();
+                    int numeroAleatorio = random.Next(0, lista.Count);
+                    Console.WriteLine(numeroAleatorio);
+
+                    //Escribimos el enunciado en el writer
+                    string enunciado = lista[numeroAleatorio].enunciado;
+                    writer.WriteLine(enunciado);
+                    Console.WriteLine("Enviado:P " + enunciado);
+                    writer.Flush();
+
+                    //Comprobamos si acierta
+                    bool acierto = false;
+
+                    while (acierto == false)
+                    {
+                        // Recibimos la respuesta del cliente
+                        var respuesta = reader.ReadLine();
+                        Console.WriteLine("Recibido:\n" + respuesta);
+                        Console.WriteLine("RespuestaReal: " + lista[numeroAleatorio].respuesta);
+
+                        // Comprobamos si las respuestas coinciden
+                        if (respuesta.ToLower().Equals(lista[numeroAleatorio].respuesta.ToLower()))
+                        {
+                            // Mandamos un OK si acierta
+                            writer.WriteLine("OK");
+                            Console.WriteLine("Enviado:R OK");
+                            
+                            // Borramos adivinanza
+                            lista.RemoveAt(numeroAleatorio);
+                            Console.WriteLine("Borrando adivinanza de la lista: " + enunciado);
+                            Console.WriteLine("Numero de adivinanzas disponibles en la lista: " + lista.Count());
+
+                            acierto = true;
+                            writer.Flush ();
+                        }
+                        else
+                        {
+                            // Mandamos un KO si no acierta
+                            Console.WriteLine("Enviado:R KO");
+                            writer.WriteLine("KO");
+                            writer.Flush ();
+                        }
+                    }                 
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0} Apagando el servidor por error", e.Message);
+            }
+            
+        }
+        /**
+         * Metodo que carga las adivinanzas del XML
+         * @return una list de adivinanzas
+         */
+        public static List<Adivinanza> CargaFichero()
+        {
+            List<Adivinanza> lista = new List<Adivinanza>();
+            string posicion = "";
+            string enunciado = "";
+            string respuesta = "";
+            
+            // Cargamos el XML
+            XmlDocument documento = new XmlDocument();
+            documento.Load("adivinanzas.xml");
+
+            // Nodo adivinanzaS
+            foreach (XmlNode nodo1 in documento.ChildNodes)
+            {
+                if (nodo1.HasChildNodes)
+                {
+                    // Nodo adivinanza 
+                    foreach (XmlNode nodo2 in nodo1.ChildNodes)
+                    {
+                        posicion = nodo2.Attributes["numero"].Value;
+                        enunciado = nodo2["enunciado"].InnerText;
+                        respuesta = nodo2["respuesta"].InnerText;
+                        
+                        Adivinanza adi = new Adivinanza(posicion,enunciado,respuesta);
+                        lista.Add(adi);
+                    }
+                }
+            }
+            return lista;
+        }
+    }
+    
+    /**
+     * Clase para gestionar las adivinanzas
+     */
+    public class Adivinanza
+    {
+        public string posicion;
+        public string enunciado;
+        public string respuesta;
+
+        public Adivinanza(string posicion, string enunciado, string respuesta)
+        {
+            this.posicion = posicion;
+            this.enunciado = enunciado;
+            this.respuesta = respuesta; 
+        }
+    }
+
+   
+}
